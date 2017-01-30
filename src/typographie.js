75cc28edc34c51889f4a844400ac775b53900a9b
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-01-30 20:30:03 +0200

    Add library sketch

--- src/typographie.js
+++ src/typographie.js
@@ -1,4 +1,11 @@
 export default class Typographie {
+	constructor(actionlist) {
+		this.actions(actionlist);
+		this._preserved = [];
+	}
+	actions(actionlist) {
+		this._actions = actionlist;
+	}
 	process(text) {
 		return text;
 	}
